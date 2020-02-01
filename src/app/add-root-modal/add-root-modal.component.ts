79bd359e43e7c7cae0a32acc9bba6920601799f6
IyiKuyoro <opeoluwa.iyikuyoro@gmail.com>
2020-02-01 06:40:18 +0100

    style component

--- src/app/add-root-modal/add-root-modal.component.ts
+++ src/app/add-root-modal/add-root-modal.component.ts
@@ -3,13 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-add-root-modal',
   templateUrl: './add-root-modal.component.html',
-  styleUrls: ['./add-root-modal.component.sass']
+  styleUrls: ['./add-root-modal.component.scss']
 })
 export class AddRootModalComponent implements OnInit {
+  rootType: 'Manager' | 'Department';
 
   constructor() { }
 
   ngOnInit() {
+    this.rootType = 'Manager';
   }
 
+  switchRootType(): void {
+    if (this.rootType === 'Manager') {
+      this.rootType = 'Department';
+    } else {
+      this.rootType = 'Manager';
+    }
+  }
 }
